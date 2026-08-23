260c867003cbe89693acb5d1813443339d1911cc
Mohd Anas <mohd.anas.gt3@gmail.com>
2026-08-23 12:03:25 +0530

    Time: 4 ms (47.55%), Space: 14.1 MB (10.26%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+        int n = num.length();
+        int leftSum = 0, rightSum = 0;
+        int leftQ = 0, rightQ = 0;
+
+        for (int i = 0; i < n / 2; ++i) {
+            if (num[i] == '?') {
+                leftQ++;
+            } else {
+                leftSum += num[i] - '0';
+            }
+        }
+
+        for (int i = n / 2; i < n; ++i) {
+            if (num[i] == '?') {
+                rightQ++;
+            } else {
+                rightSum += num[i] - '0';
+            }
+        }
+
+        
+        if ((leftQ + rightQ) % 2 != 0) {
+            return true;
+        }
+
+        
+        return (leftSum - rightSum) != (rightQ - leftQ) * 9 / 2;
+    }
+};
